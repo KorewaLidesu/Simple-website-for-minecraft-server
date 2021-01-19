e610b9c88c9a1aa16b7e66d3c4d4c4504bfe59f3
Axel Benitez <skynnoan.137@gmail.com>
2021-01-19 19:44:27 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php 
+/*
+
+╭╮╱╭╮╱╱╭┳━━━┳━━┳━━━┳━╮╱╭┳━━━┳━━━┳╮╱╱╭╮
+┃┃╱┃╰╮╭╯┃╭━╮┣┫┣┫╭━╮┃┃╰╮┃┣╮╭╮┃╭━━┫╰╮╭╯┃
+┃┃╱╰╮╰╯╭┫╰━╯┃┃┃┃┃╱┃┃╭╮╰╯┃┃┃┃┃╰━━╋╮┃┃╭╯
+┃┃╱╭╋╮╭╯┃╭╮╭╯┃┃┃╰━╯┃┃╰╮┃┃┃┃┃┃╭━━╯┃╰╯┃
+┃╰━╯┃┃┃╱┃┃┃╰┳┫┣┫╭━╮┃┃╱┃┃┣╯╰╯┃╰━━╮╰╮╭╯
+╰━━━╯╰╯╱╰╯╰━┻━━┻╯╱╰┻╯╱╰━┻━━━┻━━━╯╱╰╯
+
+*/
+
+$menu_logo = false; # Show logo in menu
+$menu_social_networks = true; # Show social networks in menu
+$show_players_online = true; # Show online players in server
+$show_server_ip = true; # Show server ip
+$show_server_port = false; # Show server port
+
+# Server store
+$show_store_link = true;
+$store_link = ""; # require 'https://'
+
+# Website background
+$website_background = "image"; # <color/image>
+
+$website_background_color = "#222"; # Work only if 'website_background' is 'color'
+$website_background_image = "background.png"; # Work only if 'website_background' is 'image'
+$hero_image = true; # Show gradient color in background image (hero image)
+
+?>
